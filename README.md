ffe3bb1ece3eedd5a4af79143044b81104619b9d
Jason Zhang <jasonjzhang17@gmail.com>
2022-06-08 15:47:50 -0400

    Abstracted authentication

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Twitter-Mining
+
+## Tweepy Stream Gist Code Example
+https://gist.github.com/bonzanini/af0463b927433c73784d
--- collect_data.py
+++ collect_data.py
@@ -1,22 +1,10 @@
 import tweepy
-from tweepy import OAuthHandler
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-CONSUMER_KEY_SECRET = os.getenv('CONSUMER_KEY_SECRET')
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
-BEARER_TOKEN = os.getenv('BEARER_TOKEN')
-
-
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
- 
-api = tweepy.API(auth)
+from utils import auth, api
 
 for status in tweepy.Cursor(api.home_timeline).items(10):
     # Process a single status
     print(status.text)
+
+# for status in tweepy.Cursor(api.home_timeline).items(10):
+#     # Process a single status
+#     print(status._json)
