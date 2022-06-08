6039a6d23dc6322ef72bba1f37fbd9f40baca68d
Jason Zhang <jasonjzhang17@gmail.com>
2022-06-08 15:18:21 -0400

    Established access to Twitter v2 API

---
+++ collect_data.py
@@ -0,0 +1,22 @@
+import tweepy
+from tweepy import OAuthHandler
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_KEY_SECRET = os.getenv('CONSUMER_KEY_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+BEARER_TOKEN = os.getenv('BEARER_TOKEN')
+
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+ 
+api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+    print(status.text)
