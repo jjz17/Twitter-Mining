c54ddd1a282a346c388735afff69e3103a5f279b
Jason Zhang <jasonjzhang17@gmail.com>
2022-06-09 16:34:00 -0400

    Parsing useful data from tweets

--- collect_data.py
+++ collect_data.py
@@ -1,6 +1,7 @@
 import tweepy
 from utils import auth, api
 import json
+from pprint import pprint
 
 
 # tweet_data = []
@@ -20,6 +21,18 @@ import json
 #     f.close()
 
 query = 'NBA'
-tweets = [tweet._json for tweet in tweepy.Cursor(api.search_tweets, q=query, lang='en', result_type='mixed', count=10).items(1)]
-for tweet in tweets:
-    print(tweet.keys())
+# Create list of dictionaries of tweet data
+tweets = [tweet._json for tweet in tweepy.Cursor(
+    api.search_tweets, q=query, lang='en', result_type='mixed', count=10).items(1)]
+# Parse out useful information from each dict
+'''
+dict_keys(['created_at', 'id', 'id_str', 'text', 'truncated', 'entities', 'metadata', 'source', 'in_reply_to_status_id',
+'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 
+'coordinates', 'place', 'contributors', 'is_quote_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 
+'possibly_sensitive', 'lang'])
+'''
+simple_tweets = [{k: tweet[k] for k in ['user', 'text', 'geo', 'coordinates',
+                                        'retweet_count', 'favorite_count']} for tweet in tweets]
+                            
+for tw in simple_tweets:
+    pprint(tw)
