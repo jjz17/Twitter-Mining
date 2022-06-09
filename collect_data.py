d247fca4cc090143926e77de3f58c96f1ceade99
Jason Zhang <jasonjzhang17@gmail.com>
2022-06-09 11:52:16 -0400

    Implemented data collection

--- collect_data.py
+++ collect_data.py
@@ -1,9 +1,34 @@
 import tweepy
 from utils import auth, api
+import json
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
+
+tweet_data = []
+for status in tweepy.Cursor(api.home_timeline).items(2):
     # Process a single status
-    print(status.text)
+    tweet_data.append(status._json)
+
+with open('empty.json', 'r') as f:
+    # Load currently stored json array
+    data = json.load(f)
+    # Append existing data with new data
+    data = data + tweet_data
+    with open('empty.json', 'w') as f2:
+        # Dump appended data to file
+        json.dump(data, f2)
+        f2.close()
+    f.close()
+
+# with open('empty.json', 'r+') as f:
+#     data = json.load(f)
+#     # If json file was empty
+#     if not data:
+#         data = data + tweet_data
+#         # Delete original contents
+#         f.truncate()
+#         with open('empty.json', 'w') as f2:
+#             json.dump(data, f2)
+#         f.close()
 
 # for status in tweepy.Cursor(api.home_timeline).items(10):
 #     # Process a single status
