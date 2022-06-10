6581c851154a76ed930319798a7fb853e5f19059
Jason Zhang <jasonjzhang17@gmail.com>
2022-06-10 16:19:59 -0400

    Applied model to perform sentiment analysis on tweets

---
+++ process_data.py
@@ -0,0 +1,82 @@
+import json
+from textwrap import indent
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+from pprint import pprint
+ 
+'''
+Example
+'''
+# with open('empty.json', 'r') as f:
+#     counter = Counter()
+#     tweets = json.load(f) # load it as a list of Python dicts
+#     for tweet in tweets:
+#         # Create a list with all the terms
+#         terms_all = [term for term in word_tokenize(tweet['text'])]
+#         # Update the counter
+#         counter.update(terms_all)
+#     # Print the first 5 most frequent words
+#     print(counter.most_common(5))
+#     # print(json.dumps(tweet, indent=4)) # pretty-print
+#     # print(type(tweets[0]), len(tweets))
+
+# with open('data.json', 'r') as f:
+#     data = json.load(f)
+#     for user, tweets in data.items():
+#         print(f'User: {user}')
+#         for tweet in tweets:
+#             print(f'Tweet: {tweet["text"]}')
+
+def get_text_data():
+    '''
+    Extract tweet texts
+    '''
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        simple_data = {}
+        for user, tweets in data.items():
+            simple_data[user] = [tweet['text'] for tweet in tweets]
+    return(simple_data)
+
+
+def preprocess_text(text: str) -> str:
+    '''
+    Preprocess tweet text for the model
+    '''
+    words = []
+    for word in text.split(' '):
+        if word.startswith('@') and len(word) > 1:
+            word = '@user'
+        elif word.startswith('http'):
+            word = "http"
+        words.append(word)
+    return ' '.join(words)
+
+
+# Load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+
+labels = ['Negative', 'Neutral', 'Positive']
+
+data = get_text_data()
+for user, tweets in data.items():
+    print(f'User: {user}')
+    for tweet in tweets:
+        print(f'Tweet: {tweet}')
+        processed_text = preprocess_text(tweet)
+
+        # sentiment analysis
+        encoded_tweet = tokenizer(processed_text, return_tensors='pt')
+        # output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+        output = model(**encoded_tweet)
+
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+
+        for label, score in zip(labels, scores):
+            print(f'\t{label}: {score}')
